4e4c08f5667f51a6c7f7e55f70411c2ead7d6922
Lisset Rico <lrico5@uic.edu>
2025-01-20 14:02:56 -0600

    new layout and palette

--- me/src/app/page.tsx
+++ me/src/app/page.tsx
@@ -1,101 +1,80 @@
 import Image from "next/image";
+import Link from 'next/link';
+import './globals.css';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <nav>
+        <ul>
+          <li><Link href="#home">home</Link></li>
+          <li><Link href="#about-me">about me</Link></li>
+          <li><Link href="#experiences">experiences</Link></li>
+          <li><Link href="#projects">projects</Link></li>
+          <li><Link href="#leadership">leadership</Link></li>
+          <li><Link href="#awards">awards</Link></li>
+          <li><Link href="#skills">skills</Link></li>
+        </ul>
+      </nav>
+      
+      <section id="home" className="content">
+        <div className="front-page">
+          <Image
+            src="/japan_self_portrait.jpg" // Make sure to place your image in the 'public' folder
+            alt="Your Name"
+            width={300} // Adjust size of the image
+            height={300} // Adjust size of the image
+            className="image"
+          />
+          <div>
+            <h1 className="name">Lisset C. Rico</h1>
+            <p className="profession">Software Engineer</p>
+            
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+
+      <section id="about-me">
+        <h1 className="title">about me</h1>
+        <p>
+          Hello! I&apos;m Lisset, a Software Engineer from Chicago, IL. 
+          I&apos;m a recent graduate from the <b>University of Illinois at Chicago</b>. I completed a <b>Bachelor&apos;s in Science of Computer Science</b> in Fall 2024.
+          I first learned about CS in the AP Computer Science A course offered at my school my sophomore year. From then on, I couldn&apos;t get coding out of my head.
+        </p>
+      </section>
+
+      <section id="experiences" className="sections">
+        <h1 className="title">experiences</h1>
+        <h3>Chicago Tech Circle</h3>
+        <p></p>
+
+        <h3>UI Health</h3>
+        <p></p>
+
+        <h3>UI</h3>
+
+        <h3>UIC Wearable Technology and Sensory Laboratory</h3>
+      </section>
+
+      <section id="projects" className="sections">
+        <h1 className="title">projects</h1>
+      </section>
+
+      <section id="leadership" className="sections">
+        <h1 className="title">leadership</h1>
+      </section>
+
+      <section id="awards" className="sections">
+        <h1 className="title">awards</h1>
+      </section>
+
+      <section id="skills" className="sections">
+        <h1 className="title">skills</h1>
+      </section>
+
+      <section id="footer" className="sections">
+        <p>built and owned by Lisset C. Rico</p>
+      </section>
     </div>
   );
 }
