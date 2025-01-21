6d1eed9fdb065fbaa343a7110a0b1dbf9645c277
Lisset Rico <lrico5@uic.edu>
2025-01-21 11:14:49 -0600

    added experiences and updated layout

---
+++ me/src/app/experiences.ts
@@ -0,0 +1,155 @@
+type Experience = {
+    company: string;
+    position: string;
+    dates: string;
+    description: string[];
+};
+
+
+export const experiences = [
+    {
+        company: "Break Through Tech Chicago",
+        position: "Student Assistant",
+        dates: "September 2024 - December 2024",
+        description: [
+            "Assisted in office operations by providing administrative support, coordinating communications, and maintaining office organization.",
+        ],
+    },
+    {
+        company: "UIC College of Engineering",
+        position: "Teaching Assistant",
+        dates: "September 2024 - December 2024",
+        description: [
+            "Led 50+ students through weekly labs for the course CS100: Discovering Computer Science.",
+            "Provided 2+hrs weekly of office hours giving students 1 to 1 meetings and study/support sessions."
+        ],
+    },
+    {
+        company: "Chicago Tech Circle",
+        position: "Quantum Research Intern",
+        dates: "May 2024 - August 2024",
+        description: [
+            "Contributed to the Quantum Foundry at Argonne National Laboratory in automating the production of diamond qubits for quantum applications with 5 undergrad interns.",
+            "Earned the Outstanding Undergraduate Poster Award at the 2024 Chicago Quantum Exchange.",
+            "Developed device drivers through virtual instrument software that remote controlled the electromagnetic etching hardware.",
+            "Utilized a machine learning library to detect un-etched membrane percentages throughout the etching process.",
+            "Integrated 15+ functions to provide proof of concept on a automated program for diamond etching.",
+            "Refined and debugged 8+ files of code while maintaining code structure."
+        ],
+    },
+    {
+        company: "Chicago Tech Circle",
+        position: "Tech Consultant",
+        dates: "January 2024 - April 2024",
+        description: [
+            "Contributed to a hardware testing initiative for Zebra Technologies, a sensing/tracking company, with 5 undergrad students.",
+            "Utilized hardware troubleshooting to conduct research, employ social engineering techniques, develop comprehensive test cases, and propose solutions aligned with their business needs."
+        ],
+    },
+    {
+        company: "Break Through Tech Chicago",
+        position: "Student Assistant",
+        dates: "September 2023 - December 2023",
+        description: [
+            "Actively represented BTTC at 10+ events, engaging as both worker and student to guide students on their journey within the chapter.",
+            "Increased summer program applicants by expanding outreach to partner student organizations' Discord servers.",
+            "Coordinated 1:1 meetings with student organizations to communicate on collaboration event goals."
+        ],
+    },
+    {
+        company: "UIC College of Engineering",
+        position: "Teaching Assistant",
+        dates: "January 2023 - April 2023",
+        description: [
+            "Led 300+ students through weekly labs for the course CS211: Programming Practicum.",
+            "Graded 100+ student laboratory assignments to support the graduate teaching assistants.",
+            "Provided 10+hrs weekly of office hours giving students 1 to 1 meetings and study/support sessions.",
+        ],
+    },
+    {
+        company: "UIC College of Engineering",
+        position: "Teaching Assistant",
+        dates: "August 2022 - December 2022",
+        description: [
+            "Led 70+ students through weekly labs for the course CS211: Programming Practicum.",
+            "Graded 50+ student laboratory assignments to support the graduate teaching assistants.",
+            "Provided 10+hrs weekly of office hours giving students 1 to 1 meetings and study/support sessions.",
+        ],
+    },
+    {
+        company: "UIC College of Engineering",
+        position: "Research Mentor",
+        dates: "August 2022 - December 2022",
+        description: [
+            "Guided sophomore research groups in the Early Research Scholars Program in developing their own research topic and paper.",
+            "Coordinated meetings with 2 research groups to provide advice and assistance into their projects.",
+        ],
+    },
+    {
+        company: "UI Health",
+        position: "Software Engineering Intern",
+        dates: "May 2022 - July 2022",
+        description: [
+            "Applied full-stack features to the 1-Search Platform to improve clinical search result performance.",
+            "Adjusted API call structure for a 50% reduction in search query response time.",
+            "Restructured the login/signup render to match the new platform layout to give a smoother user experience.",
+            "Implemented user-friendly filtering in the front-end, allowing users to refine search results.",
+            "Maintained uniform code formatting for back-end and front-end programs within 5 folders."
+        ],
+    },
+    {
+        company: "UIC College of Engineering",
+        position: "Teaching Assistant",
+        dates: "January 2022 - April 2022",
+        description: [
+            "Led 200+ students through weekly labs for the course CS141: Program Design II.",
+        ],
+    },
+    {
+        company: "UIC College of Engineering",
+        position: "Teaching Assistant",
+        dates: "August 2021 - December 2021",
+        description: [
+            "Led 90+ students through weekly labs for the course CS100: Discovering Computer Science.",
+            "Graded 90+ student laboratory assignments to support the graduate teaching assistants.",
+            "Provided 5+hrs weekly of office hours giving students 1 to 1 meetings and study/support sessions."
+        ]
+    },
+    {
+        company: "UIC College of Engineering",
+        position: "Research Intern",
+        dates: "May 2021 - August 2021",
+        description: [
+            "Researched the logistics of running a prediction market for trading and political polls.",
+            "Analyzed and discussed existing prediction model literature for possible political models.",
+        ],
+    },
+];
+  
+// const projects = [
+//     {
+//         title: "Personal Website",
+//         tech: "Next.js, React, Vercel",
+//         description: 
+//             "My personal website.",
+//     },
+// ];
+
+// const leadership = [
+//     {
+//         role: "Outreach Committee",
+//         organization: "UIC Society of Hispanic Professional Engineers",
+//         dates: "October 2024 - December 2024"
+//         description: 
+//             "My personal website.",
+//     },
+// ];
+
+// Group experiences by company
+export const groupedExperiences: { [key: string]: Experience[] } = experiences.reduce((acc, experience) => {
+    if (!acc[experience.company]) {
+      acc[experience.company] = [];
+    }
+    acc[experience.company].push(experience);
+    return acc;
+  }, {} as { [key: string]: Experience[] });
--- me/src/app/page.tsx
+++ me/src/app/page.tsx
@@ -1,8 +1,28 @@
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from 'next/link';
+import { groupedExperiences } from "./experiences";
+import { FaLinkedin, FaGithubSquare } from 'react-icons/fa';
+import { MdEmail } from "react-icons/md";
 import './globals.css';
 
+// Define the type for each experience
+type Experience = {
+  company: string;
+  position: string;
+  dates: string;
+  description: string[];
+};
+
 export default function Home() {
+  const [selectedCompany, setSelectedCompany] = useState<string | null>('Break Through Tech Chicago'); // Track selected company, set BTTC as default
+
+  const handleClick = (company: string) => {
+    setSelectedCompany((prev) => (prev === company ? null : company)); // Toggle the company details
+  };
+
   return (
     <div>
       <nav>
@@ -12,7 +32,7 @@ export default function Home() {
           <li><Link href="#experiences">experiences</Link></li>
           <li><Link href="#projects">projects</Link></li>
           <li><Link href="#leadership">leadership</Link></li>
-          <li><Link href="#awards">awards</Link></li>
+          {/* <li><Link href="#awards">awards</Link></li> */}
           <li><Link href="#skills">skills</Link></li>
         </ul>
       </nav>
@@ -20,60 +40,103 @@ export default function Home() {
       <section id="home" className="content">
         <div className="front-page">
           <Image
-            src="/japan_self_portrait.jpg" // Make sure to place your image in the 'public' folder
+            src="/japan_self_portrait.jpg" // In the public folder
             alt="Your Name"
-            width={300} // Adjust size of the image
-            height={300} // Adjust size of the image
+            width={300}
+            height={300}
             className="image"
           />
           <div>
-            <h1 className="name">Lisset C. Rico</h1>
-            <p className="profession">Software Engineer</p>
-            
+            <h1 className="name">Hi! I&apos;m Lisset C. Rico</h1>
+            <h2 className="profession">Software Engineer</h2>
+            <div className="icons">
+              <a href="https://github.com/Liz581" target="_blank" rel="noopener noreferrer">
+                <FaGithubSquare size={40} color="#778DA9" />
+              </a>
+              <a href="https://www.linkedin.com/in/lisset-c-rico-85aa91210/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin size={40} color="#778DA9" />
+              </a>
+              <a href="lissetcrico@gmail.com" target="_blank" rel="noopener noreferrer">
+                <MdEmail size={40} color="#778DA9" />
+              </a>
+            </div>
           </div>
         </div>
       </section>
 
-      <section id="about-me">
+      <section id="about-me" className="content">
         <h1 className="title">about me</h1>
         <p>
-          Hello! I&apos;m Lisset, a Software Engineer from Chicago, IL. 
+          I&apos;m Lisset, a Software Engineer from Chicago, IL. 
           I&apos;m a recent graduate from the <b>University of Illinois at Chicago</b>. I completed a <b>Bachelor&apos;s in Science of Computer Science</b> in Fall 2024.
           I first learned about CS in the AP Computer Science A course offered at my school my sophomore year. From then on, I couldn&apos;t get coding out of my head.
         </p>
       </section>
 
-      <section id="experiences" className="sections">
+      <section id="experiences" className="content">
         <h1 className="title">experiences</h1>
-        <h3>Chicago Tech Circle</h3>
-        <p></p>
 
-        <h3>UI Health</h3>
-        <p></p>
+        {/* Left column - Company List */}
+        <div className="experience-container">
+          <div className="experience-list">
+            {Object.keys(groupedExperiences).map((company, index) => (
+              <div
+                key={index}
+                className={`experience-item ${selectedCompany === company ? "active" : ""}`}
+                onClick={() => handleClick(company)}
+              >
+                {company}
+              </div>
+            ))}
+          </div>
 
-        <h3>UI</h3>
-
-        <h3>UIC Wearable Technology and Sensory Laboratory</h3>
+          {/* Right column - Details of selected experience */}
+          <div className="experience-details">
+            {selectedCompany !== null && (
+              <div className="details-content">
+                {groupedExperiences[selectedCompany].map((experience: Experience, index: number) => (
+                  <div key={index}>
+                    <h3 className="position">{experience.position}</h3>
+                    <h4 className="dates">{experience.dates}</h4>
+                    <div className="description">
+                      {experience.description.map((sentence, idx) => (
+                        <p key={idx}>{sentence}</p>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
       </section>
 
-      <section id="projects" className="sections">
+
+      <section id="projects" className="content">
         <h1 className="title">projects</h1>
+        <div className="projects-container">
+          <div className="projects-list">
+            {/* Map through projects and display them here */}
+          </div>
+        </div>
       </section>
 
-      <section id="leadership" className="sections">
+
+      <section id="leadership" className="content">
         <h1 className="title">leadership</h1>
       </section>
 
-      <section id="awards" className="sections">
+      {/* <section id="awards" className="content">
         <h1 className="title">awards</h1>
-      </section>
+      </section> */}
 
-      <section id="skills" className="sections">
+      <section id="skills" className="content">
         <h1 className="title">skills</h1>
+
       </section>
 
-      <section id="footer" className="sections">
-        <p>built and owned by Lisset C. Rico</p>
+      <section id="footer" className="content">
+        <h3 className="footers">built, managed and owned by Lisset C. Rico</h3>
       </section>
     </div>
   );
